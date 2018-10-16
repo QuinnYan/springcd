ad6730d5b07cc18b41b25abc781d7ec2e2251be9
QuinnYan <532629823@qq.com>
2018-10-16 16:42:44 +0800

    Add Upload Api & SearchApi

--- src/main/java/com/kdk/springcd/service/implement/VideoServiceImpl.java
+++ src/main/java/com/kdk/springcd/service/implement/VideoServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.io.File;
+import java.io.FileOutputStream;
 
 
 @Service
@@ -18,18 +20,44 @@ public class VideoServiceImpl implements VideoService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private VideoInfo setVideoInfo(Map<String,Object> map){
+        VideoInfo videoInfo = new VideoInfo();
+        videoInfo.setVideoID(map.get("video_id").toString());
+        videoInfo.setVideoURL(map.get("video_url").toString());
+        videoInfo.setVideoCollection((Integer)map.get("video_collection"));
+        videoInfo.setUserID(map.get("user_id").toString());
+        videoInfo.setVideoLike((Integer)map.get("video_like"));
+        videoInfo.setVideoDescription(map.get("video_description").toString());
+
+        return videoInfo;
+    }
+
     @Override
     public VideoInfo getVideoByID(String video_id){
 
-        VideoInfo videoInfo = new VideoInfo();
         Map<String,Object> map = jdbcTemplate.queryForMap("select * from video_all where video_id = ? " ,video_id);
-        videoInfo.setVideoID(map.get("video_id").toString());
-        videoInfo.setVideoURL(map.get("video_url").toString());
-        videoInfo.setVideoCollection(map.get("video_collection").toString());
-        videoInfo.setUserID(map.get("user_id").toString());
-        videoInfo.setVideoLike((Integer)map.get("video_like"));
+        VideoInfo videoInfo = setVideoInfo(map);
+//        videoInfo.setVideoID(map.get("video_id").toString());
+//        videoInfo.setVideoURL(map.get("video_url").toString());
+//        videoInfo.setVideoCollection(map.get("video_collection").toString());
+//        videoInfo.setUserID(map.get("user_id").toString());
+//        videoInfo.setVideoLike((Integer)map.get("video_like"));
 
         return videoInfo;
+    }
+
+    @Override
+    public List<VideoInfo> getVideoByName(String video_name){
+        List<VideoInfo> resList = new ArrayList<>();
+        video_name = "%" + video_name + "%";
+        String sql = "select * from video_all where video_name like ? ";
+        List<Map<String,Object>> objList = jdbcTemplate.queryForList(sql,video_name);
+
+        for (Map<String,Object> map : objList){
+            VideoInfo videoInfo = setVideoInfo(map);
+            resList.add(videoInfo);
+        }
+        return resList;
     }
 
     @Override
@@ -39,13 +67,7 @@ public class VideoServiceImpl implements VideoService {
        List<VideoInfo> recVideoList = new ArrayList<>();
        List<Map<String,Object>> objList = jdbcTemplate.queryForList(sql,video_num);
        for(Map<String,Object> map : objList){
-           VideoInfo videoInfo = new VideoInfo();
-           videoInfo.setVideoID(map.get("video_id").toString());
-           videoInfo.setVideoURL(map.get("video_url").toString());
-           videoInfo.setVideoCollection(map.get("video_collection").toString());
-           videoInfo.setUserID(map.get("user_id").toString());
-           videoInfo.setVideoLike((Integer)map.get("video_like"));
-
+           VideoInfo videoInfo = setVideoInfo(map);
            recVideoList.add(videoInfo);
        }
        return recVideoList;
@@ -99,5 +121,24 @@ public class VideoServiceImpl implements VideoService {
         //  在连接表中加入条目
         sql = "insert into video_collect_person (video_id, user_id) values(?,?)";
         jdbcTemplate.update(sql,video_id,user_id);
+    }
+
+    @Override
+    public void uploadVideo(byte[] file, String filePath, String fileName, String video_description, String user_ID) throws Exception{
+        File targetFile = new File(filePath);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        FileOutputStream out = new FileOutputStream(filePath+fileName);
+        out.write(file);
+        out.flush();
+        out.close();
+
+        String fileUrl = "";
+        /*
+        设置url为本地保存的路径
+         */
+        String sql = "INSERT INTO video_all (`video_description`, `video_url`, `user_id`) VALUES (?,?,?)";
+        jdbcTemplate.update(sql, video_description, fileUrl, user_ID);
     }
 }
